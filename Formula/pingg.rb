e3f78f6acb4951f2e394bb59693c2d9707693d5f
SZN <szymon@nieradka.net>
2024-05-01 20:50:12 +0200

    homepage update

--- Formula/pingg.rb
+++ Formula/pingg.rb
@@ -1,6 +1,6 @@
 class Pingg < Formula
-  desc "Graphical ping utility to monitor your internet connection quality"
-  homepage "https://github.com/szn/pingg"
+  desc "Graphical ping utility to monitor your internet connection"
+  homepage "https://pingg.nieradka.net"
   url "https://github.com/szn/pingg/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "fa16137cf53f37c3f08ac2133a2bf6f76d0996a5e71d66a966bdedce0ae53d5e"
   license "Apache"
