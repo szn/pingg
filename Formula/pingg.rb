73a4864fec2cf778771eec6b9c2b67a2b6b67693
SZN <szymon@nieradka.net>
2024-05-01 20:30:23 +0200

    Update formula url and sha256

--- Formula/pingg.rb
+++ Formula/pingg.rb
@@ -1,8 +1,8 @@
 class Hello < Formula
   desc "Graphical ping utility to monitor your internet connection quality"
   homepage "https://github.com/szn/pingg"
-  url "..."
-  sha256 "..."
+  url "https://github.com/szn/pingg/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ad9ac0b41972c9c6763c9a4de46fdd4d37df40710c1afe604b6f231f62ee58d6"
   license "Apache"
 
   def install
